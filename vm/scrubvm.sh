523b9ad2695ab2edfb785e462f76e4a797ada102
Craig Brozefsky <craig@tick.chi.matasano.com>
2010-03-24 17:15:00 -0500

    We can now build VMs

--- vm/scrubvm.sh
+++ vm/scrubvm.sh
@@ -1,34 +1,26 @@
 #!/usr/bin/env bash
 
 set -e
-
-# remove openssh server and re-install 
-dpkg -P build-essential
-dpkg -P linux-headers-$(uname -r)
-
-# grab pico, for our lusers
+# grab pico, for our users
 apt-get install nano
 
 apt-get clean
 
-rm -f ~playbook/initial_vm_config_done
+rm -f ~flint/initial_vm_config_done
 
 rm -rf ~root/.ssh
 
-rm -f ~root/playbook*.deb
+rm -rf ~flint/.ssh
 
-rm -rf ~playbook/.ssh
-
-rm -f ~playbook/.bash_history
+rm -f ~flint/.bash_history
 rm -f ~root/.bash_history
 
-touch ~playbook/.password_needs_changing
-chown playbook ~playbook/.password_needs_changing
+rm -f ~flint/flint/app/app_secret.txt
 
 # lock root account
 passwd -l root
 
-rm -f ~root/prepvm.sh
+rm -f ~flint/prepvm.sh
 
 # this zeros out the remainder of the FS, making it compress better
 echo "Zeroing out remainder of disk, to make compression easier"
